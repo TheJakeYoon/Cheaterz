8f69101615934d8efe79ef52afbbd96d02b59a11
Kellan Cerveny <kjcerveny@gmail.com>
2020-04-29 23:25:29 -0500

    Arguments for the main program and additional file for finding file names in directory

---
+++ driver.cpp
@@ -0,0 +1,25 @@
+// Driver for Cheaters program
+// Kellan Cerveny and Jake Yoon
+// Priebe 312 4/19/2020
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std; 
+
+int main(int argc, char* argv[]){
+    cout << "Argc: " << argc << endl;
+    cout << argv[0] << endl;
+    cout << argv[1] << endl;
+    cout << argv[2] << endl;
+    cout << argv[3] << endl;
+    // Milestone I: take commands [file path, len of word sequence]
+    // Determine how many files are in the given directory
+    // Create a hash table based on the number of files (how long does this need to be?)
+    // function to hash the text files
+    // compare to find repeated sequences in files
+
+
+	return 0;
+}
